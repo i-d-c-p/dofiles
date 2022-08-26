cb1cbf1f0539c7ccb8fb102c14e58f312f4388a8
Ivan C <idcp0496@gmail.com>
2022-08-26 20:21:00 +0200

    Add initial configuration

---
+++ nvim/config/plugins_config.lua
@@ -0,0 +1,141 @@
+--- TODOS CONFIGURATION
+require("todo-comments").setup {}
+
+--- ADD TREESITTER CONFIG
+require'nvim-treesitter.configs'.setup {
+	--ensure_installed = { "go" },
+
+	sync_install = false,
+
+	auto_install = true,
+
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+}
+
+--- SETUP SNIPPETS
+require'luasnip'.filetype_extend("go", {"go"})
+
+--- SETUP CMP  
+local cmp = require'cmp'
+
+cmp.setup({
+	snippet = {
+		--- REQUIRED
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), --- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+--- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' },
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
+--- SETUP GOLANG 
+-- Setup LSP
+vim.api.nvim_create_autocmd("BufWritePre",
+	{ pattern = { "*.go"}, command = "lua vim.lsp.buf.formatting()"}
+)
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.gopls.setup{
+	capabilities = capabilities,
+	settings = {
+	      gopls = {
+		      experimentalPostfixCompletions = true,
+		      analyses = {
+		        unusedparams = true,
+		        shadow = true,
+		     },
+		     staticcheck = true,
+		 },
+	},
+
+	on_attach = function()
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, {buffer=0})
+		vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {buffer=0})
+		vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {buffer=0})
+		vim.keymap.set('n', '<leader>cr', vim.lsp.buf.formatting, {buffer=0})
+	end,
+}
+
+--- SETUP DAP
+vim.keymap.set("n", "<F1>", ":lua require'dap'.continue()<cr>")
+vim.keymap.set("n", "<F2>", ":lua require'dap'.step_into()<cr>")
+vim.keymap.set("n", "<F3>", ":lua require'dap'.step_over()<cr>")
+vim.keymap.set("n", "<F4>", ":lua require'dap'.step_out()<cr>")
+vim.keymap.set("n", "<F5>", ":lua require'dap'.disconnect()<cr> :lua require'dap'.close()<cr>")
+vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<cr>")
+vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition:'))<cr>")
+
+require("nvim-dap-virtual-text").setup()
+require("dapui").setup()
+require("dap-go").setup()
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+--- THEME SETUP
+local sign = vim.fn.sign_define
+
+sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
+sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
+sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
+
---
+++ nvim/init.vim
@@ -0,0 +1,105 @@
+"-- PLUGINS
+call plug#begin('~/.config/nvim/plugged')
+	Plug 'vim-airline/vim-airline'
+	Plug 'vim-airline/vim-airline-themes'
+	Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+	Plug 'neovim/nvim-lspconfig'
+	Plug 'tpope/vim-fugitive'
+
+	" Themes
+	Plug 'rose-pine/neovim', {'as': 'rose-pine'}
+
+	" Autocompletion engine
+	Plug 'hrsh7th/cmp-nvim-lsp'
+	Plug 'hrsh7th/cmp-buffer'
+	Plug 'hrsh7th/cmp-path'
+	Plug 'hrsh7th/cmp-cmdline'
+	Plug 'hrsh7th/nvim-cmp'
+	Plug 'L3MON4D3/LuaSnip'
+	Plug 'saadparwaiz1/cmp_luasnip'
+	Plug 'rafamadriz/friendly-snippets'
+
+	" Telescope
+	Plug 'nvim-lua/plenary.nvim'
+	Plug 'nvim-telescope/telescope.nvim'
+
+	"Todos comments
+	Plug 'folke/todo-comments.nvim'
+
+	" Nvim DAP
+	Plug 'mfussenegger/nvim-dap'
+	Plug 'rcarriga/nvim-dap-ui'
+	Plug 'theHamsta/nvim-dap-virtual-text'
+	Plug 'leoluz/nvim-dap-go'
+	Plug 'ray-x/guihua.lua'
+call plug#end()
+
+"--- IMPORTING LUA FILES
+luafile $HOME/.config/nvim/config/plugins_config.lua
+
+"--- STYLE
+set number
+set laststatus=3
+highlight WinSeparator guibg=None
+colorscheme rose-pine 
+hi Normal guibg=NONE ctermbg=NONE
+hi EndOfBuffer guibg=NONE ctermbg=NONE
+
+"--- VARIABLES
+set mouse=a
+set clipboard=unnamed
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set noexpandtab
+set smartindent
+set smarttab
+
+"--- KEYBINDINGS-NAVIGATION
+nnoremap <silent><c-j> }
+nnoremap <silent><c-k> {
+vnoremap <silent><c-j> }
+vnoremap <silent><c-k> {
+nnoremap <silent><c-h> b
+nnoremap <silent><c-l> e
+vnoremap <silent><c-h> b
+vnoremap <silent><c-l> e
+
+nnoremap <silent><c-a> 0
+nnoremap <silent><c-d> $
+vnoremap <silent><c-a> 0
+vnoremap <silent><c-d> $
+
+inoremap <silent><c-h> <left>
+inoremap <silent><c-l> <right>
+inoremap <silent><c-k> <up>
+inoremap <silent><c-j> <down>
+
+"--- KEYBINDINGS-BUFFERS
+nnoremap << :bp<cr>
+nnoremap >> :bn<cr>
+
+"--- KEYBINDINGS-MODES
+inoremap jj <Esc>
+
+"--- KEYBINDINGS-TELESCOPE
+nnoremap <leader>ff <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+nnoremap <leader>fd <cmd>Telescope diagnostics<cr>
+
+"--- KEYBINDINGS-SAVE FILE
+nnoremap <silent><c-s> :<c-u>update<cr>
+vnoremap <silent><c-s> <c-c>:update<cr>gv
+inoremap <silent><c-s> <c-o>:update<cr><Esc>
+
+"--- VIM-AIRLINE CONFIG
+let g:airline_powerline_fonts = 1
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#tabline#left_sep = ' '
+let g:airline#extensions#tabline#left_alt_sep = '|'
+
+"--- AUTOMCOMPLETION
+set completeopt=menu,menuone,noselect
+
